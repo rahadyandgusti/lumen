0f094effb0835bafdb9b9a227237ccfc1daf60be
Rahadyan D Gusti <rahadyan@tlab.co.id>
2017-12-24 00:32:24 +0700

    add google analytic

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -303,4 +303,14 @@ crossorigin="anonymous"></script>
 	});
 </script>
 @yield('scripts')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-111491245-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-111491245-1');
+</script>
+
 </html>
